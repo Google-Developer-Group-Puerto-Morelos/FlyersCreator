3e9118e05529ed60f47f1db7b6bc241a96630273
Henry Raygan <contactoraygan@gmail.com>
2021-03-12 21:51:11 -0500

    Add Banner Theme for OWASP community

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,33 +18,70 @@ const screenshot = (name) => {
 
 const getTitleToSave = () => (document.getElementById("getTitle").value).replace(" ", "_");
 
+const dateToObject = (date) => {
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    const selectedDate = new Date(date);
+
+    return {
+        day: selectedDate.getDate(),
+        month: meses[selectedDate.getMonth()]
+    }
+}
+
+const hourToString = (date, city) => {
+    const selectedDate = new Date(date);
+    return `a las ${selectedDate.getHours()}:${selectedDate.getMinutes()} hrs, hora de ${city}`;
+}
+
+const setTheme = (element, theme) => {
+    const themes = {
+        owasp: {
+            src: "/assets/images/owasp-background.png",
+        },
+        gdg: {
+            src: "/assets/images/gdg-background.png"
+        }
+    }
+    element.style.backgroundImage = `url('${themes[theme].src}')`;
+    element.className = "";
+    element.classList.add(`theme-${theme}`);
+}
+
 const form = document.getElementById('formflyer');
 const data = new FormData(form);
+const triggerTheme = document.getElementById("changeTheme");
+const backgroundTheme = document.getElementById("flyerTheme");
+const appendTitle = document.getElementById("title");
+const appendSpeakerName = document.getElementById("speaker");
+const appendDayDate = document.getElementById("dayevent");
+const appendMonthDate = document.getElementById("monthevent");
+const appendHourDate = document.getElementById("hourformat");
+
+
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     const { title, speaker, date, city } = Object.fromEntries(new FormData(event.target));
-    const dateTextFormat = dateToString(date, city);
-    const appendTitle = document.getElementById("title");
-    const appendSpeakerName = document.getElementById("speaker");
-    const appendEventDate = document.getElementById("dateformat");
-
-    console.log(date);
+    const hourTextFormat = hourToString(date, city);
+    const { day, month } = dateToObject(date)
 
     appendTitle.innerText = title;
     appendSpeakerName.innerText = speaker;
-    appendEventDate.innerText = dateTextFormat;
+    appendHourDate.innerText = hourTextFormat;
+    appendDayDate.innerText = day;
+    appendMonthDate.innerText = month;
+
 
     document.getElementById("getImage").style.display = "block";
 });
 
-
 document.getElementById("getImage").style.display = "none";
+setTheme(backgroundTheme, triggerTheme.value)
+triggerTheme.addEventListener("change", () => {
+    setTheme(backgroundTheme, triggerTheme.value)
+})
 
 
-const dateToString = (date, city) => {
-    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    const selectedDate = new Date(date);
 
-    return `${selectedDate.getDate()} de ${meses[selectedDate.getMonth()]} a las ${selectedDate.getHours()}:${selectedDate.getMinutes()} hrs, hora de ${city}`;
-}
+
