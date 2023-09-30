0cbfb3fb5bca5ef15aa29ebb09a13522f257bd2c
Henry Ramirez <contactoraygan@gmail.com>
2023-09-29 19:47:14 -0600

    add new ui for theme owasp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,67 @@
-
 const screenshot = (name) => {
-    var container = document.getElementById("flyer"); // full page 
-    html2canvas(container, {
-        useCORS: true,
-        allowTaint: true,
-        scale: 1,
-        dpi: 500,
-    }).then(function (canvas) {
-        var link = document.createElement("a");
-        document.body.appendChild(link);
-        link.download = name + ".png";
-        link.href = canvas.toDataURL("image/png");
-        link.target = '_blank';
-        link.click();
-    });
-}
+  const container = document.getElementById("flyer");
+  html2canvas(container, {
+    useCORS: true,
+    allowTaint: true,
+    scale: 1,
+    dpi: 500,
+  }).then((canvas) => {
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    link.download = `${name}.png`;
+    link.href = canvas.toDataURL("image/png");
+    link.target = "_blank";
+    link.click();
+  });
+};
 
-const getTitleToSave = () => (document.getElementById("getTitle").value).replace(" ", "_");
+const getTitleToSave = () => {
+  return document.getElementById("getTitle").value.replace(" ", "_");
+};
 
 const dateToObject = (date) => {
-    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    const selectedDate = new Date(date);
+  const meses = [
+    "ENERO",
+    "FEB",
+    "MAR",
+    "ABRIL",
+    "MAY",
+    "JUNIO",
+    "JULIO",
+    "AGOSTO",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DIC",
+  ];
+  const selectedDate = new Date(date);
 
-    return {
-        day: selectedDate.getDate(),
-        month: meses[selectedDate.getMonth()]
-    }
-}
+  return {
+    day: selectedDate.getDate(),
+    month: meses[selectedDate.getMonth()],
+  };
+};
 
 const hourToString = (date, city) => {
-    const selectedDate = new Date(date);
-    return `a las ${selectedDate.getHours()}:${selectedDate.getMinutes()} hrs, hora de ${city}`;
-}
+  const selectedDate = new Date(date);
+  return `a las ${selectedDate.getHours()}:${selectedDate.getMinutes()} hrs en ${city}`;
+};
 
 const setTheme = (element, theme) => {
-    const themes = {
-        owasp: {
-            src: "./assets/images/owasp-background.png",
-        },
-        gdg: {
-            src: "./assets/images/gdg-background.png"
-        }
-    }
-    element.style.backgroundImage = `url('${themes[theme].src}')`;
-    element.className = "";
-    element.classList.add(`theme-${theme}`);
-}
+  const themes = {
+    owasp: {
+      src: "./assets/images/owasp-background.png",
+    },
+    gdg: {
+      src: "./assets/images/gdg-background.png",
+    },
+  };
+  element.style.backgroundImage = `url('${themes[theme].src}')`;
+  element.className = "";
+  element.classList.add(`theme-${theme}`);
+};
 
-const form = document.getElementById('formflyer');
-const data = new FormData(form);
+const form = document.getElementById("formflyer");
 const triggerTheme = document.getElementById("changeTheme");
 const backgroundTheme = document.getElementById("flyerTheme");
 const appendTitle = document.getElementById("title");
@@ -56,34 +69,32 @@ const appendSpeakerName = document.getElementById("speaker");
 const appendDayDate = document.getElementById("dayevent");
 const appendMonthDate = document.getElementById("monthevent");
 const appendHourDate = document.getElementById("hourformat");
-
+const getImageButton = document.getElementById("getImage");
 
 const host = window.location.host;
 
+const updateFlyer = (data) => {
+  const { title, speaker, date, city } = data;
+  const hourTextFormat = hourToString(date, city);
+  const { day, month } = dateToObject(date);
 
+  appendTitle.innerText = title;
+  appendSpeakerName.innerText = speaker;
+  appendHourDate.innerText = hourTextFormat;
+  appendDayDate.innerText = day;
+  appendMonthDate.innerText = month;
 
+  getImageButton.style.display = "block";
+};
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const { title, speaker, date, city } = Object.fromEntries(new FormData(event.target));
-    const hourTextFormat = hourToString(date, city);
-    const { day, month } = dateToObject(date)
-
-    appendTitle.innerText = title;
-    appendSpeakerName.innerText = speaker;
-    appendHourDate.innerText = hourTextFormat;
-    appendDayDate.innerText = day;
-    appendMonthDate.innerText = month;
-
-    document.getElementById("getImage").style.display = "block";
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = Object.fromEntries(new FormData(event.target));
+  updateFlyer(formData);
 });
 
-document.getElementById("getImage").style.display = "none";
-setTheme(backgroundTheme, triggerTheme.value)
+getImageButton.style.display = "none";
+setTheme(backgroundTheme, triggerTheme.value);
 triggerTheme.addEventListener("change", () => {
-    setTheme(backgroundTheme, triggerTheme.value)
-})
-
-
-
-
+  setTheme(backgroundTheme, triggerTheme.value);
+});
